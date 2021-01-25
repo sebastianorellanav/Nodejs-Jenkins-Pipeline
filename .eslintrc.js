1ec547d06955c290419bfe583a3bfd22ab81f014
sebastianorellanav <sebastian.orellana.v@usach.cl>
2021-01-25 08:27:49 -0300

    se arregla el linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,14 @@
 module.exports = {
     "env": {
-        "node": true,
+        "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "rules": {
-        "brace-style": [
-            "error",
-            "stroustrup"
-        ],
-        "comma-dangle": [
-            "error",
-            "never"
-        ],
-        "no-unused-vars": [
-            "warn"
-        ],
-        "no-var": [
-            "off"
-        ],
-        "one-var": [
-            "off"
-        ]
+        "semi": ["warn", "always"],
+        "quotes": ["warn", "double"]
     }
 };
